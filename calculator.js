6d894c88c08c9dfeb08ed593bc48a4a90bfb1a11
cstephenson <chelsea.stephenson@bettercloud.com>
2023-12-11 15:58:09 -0500

    adding new branch - calculator bonus features

--- calculator.js
+++ calculator.js
@@ -1,28 +1,44 @@
-console.log("Welcome to Calculator!")
+console.log("Welcome to Calculator!");
 const rl = require(`readline-sync`);
 
-// Ask the user for the first number.
-let firstNum = parseInt(rl.question(`Please type in your first number:\n`));
+function invalidNumber(number) {
+  return number.trimStart() === `` || Number.isNaN(Number(number));
+}
 
+// Ask the user for the first number.
+let firstNum = rl.question(`Please type in your first number:\n`);
+
+while (invalidNumber(firstNum)) {
+  firstNum = rl.question(`Hmm, that doesn't look like a valid number.`);
+}
 
 // Ask the user for the second number.
-let secondNum = parseInt(rl.question(`Please type in your second number:\n`));
-console.log(`${firstNum} ${secondNum}`)
+let secondNum = rl.question(`Please type in your second number:\n`);
+
+while (invalidNumber(secondNum)) {
+  secondNum = rl.question(`Hmm, that doesn't look like a valid number.`);
+}
+console.log(`${firstNum} ${secondNum}`);
 
 // Ask the user for an operation to perform.
 let operator = rl.question(`What operator do you want to use on these numbers:\n`);
 
+while (![`+`, `-`, '*', `/`, `**`].includes(operator)) {
+  operator = rl.question(`Must choose: +, -, *, /, or **`);
+}
+
 // Perform the operation on the two numbers.
 
-function operation(operator){
+function operation(operator) {
   switch (operator) {
-    case `+`: return firstNum+secondNum 
-    case `-`: return firstNum-secondNum
-    case `*`: return firstNum*secondNum
-    case `/`: return firstNum/secondNum
-    case `**`: return firstNum**secondNum
-
-}}
+    case `+` : return firstNum + secondNum;
+    case `-` : return firstNum - secondNum;
+    case `*` : return firstNum * secondNum;
+    case `/` : return firstNum / secondNum;
+    case `**`: return firstNum ** secondNum;
+    default: return console.log(`Must be an operation.`);
+  }
+}
 
 let answer = operation(operator);
 // Print the result to the terminal.
